394657f93b9d9d4d8e9bfce1b05f8e7053311e87
Prem Rajah <prajah@ctlondon.com>
2018-02-08 15:51:40 +0000

    finish socket io, start async and improvement

--- frameworks/server.js
+++ frameworks/server.js
@@ -5,6 +5,8 @@ var app = express()
 var http = require('http').Server(app)
 // setup in backend
 var io = require('socket.io')(http)
+// for mongo db
+var mongoose = require('mongoose')
 
 
 // serving pages
@@ -12,36 +14,45 @@ app.use(express.static(__dirname))
 
 //middle ware
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
-//placeholder messages list
-var messages = [
-  {
-    name: 'Prem Rajah',
-    message: 'Hello'
-  },
-  {
-    name: 'Lara Croft',
-    message: 'Hi there'
-  }
-]
+var dbUrl = 'mongodb://zinox:xoniz@ds229418.mlab.com:29418/learning-node101'
+
+var Message = mongoose.model('Message', {
+  name: String,
+  message: String
+})
+
 
 // get requests
 app.get('/messages', (req, res) => {
-  res.send(messages)
+
+  Message.find({}, (err, messages) => {
+    res.send(messages)
+  })
 })
 
 // post request
 app.post('/messages', (req, res) => {
-  //  check what data in the body
-  // console.log(res.body)
-  messages.push(req.body)
-  io.emit('message', req.body)
-  res.sendStatus(200)
+  var message = new Message(req.body)
+  // for mongodb (mongoose)
+  message.save((err) => {
+    if (err)
+      sendStatus(500)
+    io.emit('message', req.body)
+    res.sendStatus(200)
+  })
+
 })
 
 io.on('connection', (socket) => {
   console.log("A user connected")
+})
+
+mongoose.connect(dbUrl, (err) => {
+  console.log('Mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
